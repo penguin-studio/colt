79550f5a61b0b72a9fd07c65ab04192f541a633c
Polomkin A <polomkinanton@gmail.com>
2016-02-22 17:12:24 +0200

    add sass template

---
+++ config.rb
@@ -0,0 +1,27 @@
+# Require any additional compass plugins here.
+# require 'autoprefixer-rails'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :compressed
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+# Autoprefixer
+# on_stylesheet_saved do |file|
+	#css = File.read(file)
+	#File.open(file, 'w') do |io|
+		#io << AutoprefixerRails.process(css, browsers: ['last 2 versions'])
+	#end
+#end
