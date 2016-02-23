35ba1a6c5bf74bebbeeadd128b5b6827af8b23e9
Polomkin A <polomkinanton@gmail.com>
2016-02-23 12:00:16 +0200

    add fields

--- js/common.js
+++ js/common.js
@@ -1,3 +1,11 @@
 (function(){
-
+    var mpSliderBtn = $('.go-to-down');
+    mpSliderBtn.click( function(e){
+        e.preventDefault();
+        var scroll_el = $(this);
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+        }
+        return false;
+    });
 })();
